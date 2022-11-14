08576a30bd8cf34940ccc3b41502a0ea7d7eabec
Minerva08 <elavl@naver.com>
2022-11-15 01:56:15 +0900

    first Commit

---
+++ src/main/java/com/example/fastcampusmysql/domain/post/service/PostReadService.java
@@ -0,0 +1,80 @@
+package com.example.fastcampusmysql.domain.post.service;
+
+import com.example.fastcampusmysql.domain.post.dto.DailyPostCount;
+import com.example.fastcampusmysql.domain.post.entity.Post;
+import com.example.fastcampusmysql.domain.post.repository.PostRepository;
+import com.example.fastcampusmysql.domain.post.dto.DailyPostCountRequest;
+import com.example.fastcampusmysql.util.CursorRequest;
+import com.example.fastcampusmysql.util.PageCursor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class PostReadService {
+    final private PostRepository postRepository;
+
+    public List<Post> getPosts(Long memberId) {
+        return postRepository.findByMemberId(memberId);
+    }
+
+    public List<Post> getPosts(List<Long> postIds) {
+        return postRepository.findAllByIdIn(postIds);
+    }
+
+    public PageCursor<Post> getPosts(List<Long> memberIds, CursorRequest cursorRequest) {
+        var posts = findAllBy(memberIds, cursorRequest);
+        long nextKey = getNextKey(posts);
+        return new PageCursor<Post>(cursorRequest.next(nextKey), posts);
+    }
+
+    public List<DailyPostCount> getDailyPostCounts(DailyPostCountRequest request) {
+        return postRepository.groupByCreatedDate(request);
+    }
+
+    public Page<Post> getPosts(Long memberId, PageRequest pageRequest) {
+        return postRepository.findAllByMemberId(memberId, pageRequest);
+    }
+
+    public PageCursor<Post> getPosts(Long memberId, CursorRequest cursorRequest) {
+        var posts = findAllBy(memberId, cursorRequest);
+        long nextKey = getNextKey(posts);
+        return new PageCursor<>(cursorRequest.next(nextKey), posts);
+    }
+
+    private long getNextKey(List<Post> posts) {
+        return posts.stream()
+                .mapToLong(Post::getId)
+                .min()
+                .orElse(CursorRequest.NONE_KEY);
+    }
+
+    private List<Post> findAllBy(Long memberId, CursorRequest cursorRequest) {
+        if (cursorRequest.hasKey()) {
+            return postRepository.findAllByLessThanIdAndMemberIdAndOrderByIdDesc(
+                    cursorRequest.key(),
+                    memberId,
+                    cursorRequest.size()
+            );
+        }
+
+        return postRepository.findAllByMemberIdAndOrderByIdDesc(memberId, cursorRequest.size());
+    }
+
+    private List<Post> findAllBy(List<Long> memberIds, CursorRequest cursorRequest) {
+        if (cursorRequest.hasKey()) {
+            return postRepository.findAllByLessThanIdAndMemberIdInAndOrderByIdDesc(
+                    cursorRequest.key(),
+                    memberIds,
+                    cursorRequest.size()
+            );
+        }
+
+        return postRepository.findAllByMemberIdInAndOrderByIdDesc(memberIds, cursorRequest.size());
+    }
+
+}
